c7d8e90bb8c6fd5d0e912c2a2f46a9807d894723
eshwarcs114 <core.editor>
2021-12-08 07:22:18 +0530

    commit

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,11 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
-    // Write your code here
+    if (typeof mass === "number" and typeof height === "number" and mass>0 and height>0){
+        var bmi = mass/(height * height) 
+        return bmi
+    } else {
+        return 'INVALID INPUT'
+    }
 }
 
 module.exports = BMICalculator;
